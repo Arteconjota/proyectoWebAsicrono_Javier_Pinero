c44a5b9965efe57d4a3f1b7a57f4cd244ab91182
Arteconjota <javier.piserrano@gmail.com>
2025-04-30 01:14:54 +0200

    fin app

--- proyecto-web-async/src/main.js
+++ proyecto-web-async/src/main.js
@@ -35,25 +35,62 @@ menuButton.addEventListener("click", () => {
 const apiUrl = "https://api.unsplash.com/search/photos";
 const keyApi = "wnJXpQ-TJgCefPwUNdsl5-HRTPEsgR4GJkhBh1f8woM";
 
-const searchPhotos = (keyword) => {
-  fetch(
-    `${apiUrl}?client_id=${keyApi}&page=1&per_page=20&query=${keyword}`
-  )
-  
-  .then((res) => res.json()).then((res) => {
-    const photos = res.results;
-    printPhotos(photos);
-  })
+let currentPage = 1;
+let currentKeyword = "random";
+let isLoading = false;
+
+const searchPhotos = (keyword, page = 1) => {
+  return fetch(`${apiUrl}?client_id=${keyApi}&page=${page}&per_page=25&query=${keyword}`)
+    .then((res) => res.json())
+    .then((res) => res.results);
 };
 
 const printPhotos = (photos) => {
   const container = document.querySelector("#container");
+  if (photos.length === 0 && currentPage === 1) {
+    message.style.display = "block";
+    message.innerHTML = "No se han encontrado resultados, pruebe con otra búsqueda.";
+  } else {
+    message.style.display = "none";
+  }
+
   for (const photo of photos) {
-    container.innerHTML +=
-    `<div id="box">
-      <img src="${photo.urls.regular}" alt="${photo.alt_description}"></img>
-    </div>`
-  };
+    container.innerHTML += `
+      <div id="box">
+        <img src="${photo.urls.regular}" alt="${photo.alt_description}" />
+      </div>`;
+  }
 };
 
-searchPhotos("cat");
+// Búsqueda inicial
+searchPhotos(currentKeyword, currentPage).then(printPhotos);
+
+const message = document.querySelector("#message");
+const input = document.querySelector("#buscador");
+const container = document.querySelector("#container");
+
+input.addEventListener("keydown", async (event) => {
+  if (event.key === 'Enter') {
+    const value = input.value.trim();
+    if (value !== "") {
+      currentKeyword = value;
+      currentPage = 1;
+      container.innerHTML = "";
+      const photos = await searchPhotos(currentKeyword, currentPage);
+      printPhotos(photos);
+    }
+  }
+});
+
+// Scroll infinito
+window.addEventListener("scroll", async () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 50 && !isLoading) {
+    isLoading = true;
+    currentPage++;
+    const newPhotos = await searchPhotos(currentKeyword, currentPage);
+    printPhotos(newPhotos);
+    isLoading = false;
+  }
+});
